48bdd58c70c50ae3f90941f61d22b3098ae257c4
mehdibehvar <mehdibehvar00@gmail.com>
2023-01-30 21:34:33 +0330

    style:login-page

--- src/components/common/DashedCircle.tsx
+++ src/components/common/DashedCircle.tsx
@@ -1,20 +1,15 @@
-import React from 'react'
-import Image from "next/image";
-const DashedCircle = ({className,imageSrc}:{className:string,imageSrc?:string}) => {
-  return (
-    <div className={`${className} d-flex align-items-center justify-content-center`}>
-        <div className="inner d-flex align-items-center justify-content-center w-100 h-100">
-            {imageSrc&&<div className="image_wrapper  position-relative">
-            <Image
-              src={`${imageSrc}`}
-              alt="vitsell-login"
-              fill
-            />
-          </div>}
-          
-        </div>
-      </div>
-  )
-}
+import React from "react";
 
-export default DashedCircle
+const DashedCircle = ({ className }: { className: string }) => {
+  return (
+    <>
+      <div className={`${className}`}></div>
+      {className === "big_dashed_border" && <div className="inner"></div>}
+      {className === "litle_dashed_border" && (
+        <div className="little_inner"></div>
+      )}
+    </>
+  );
+};
+
+export default DashedCircle;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,20 +5,29 @@ import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 import { Container, Row } from "reactstrap";
 import FluidLayout from "../../components/layouts/FluidLayout";
+import Image from "next/image";
 
 
 const Login = () => {
   return (
   <>
-    <section className="Login_desktop bg-main w-100 vh-100 d-flex align-items-center d-none d-sm-flex">
-    <div className="card_wrapper">
+    <section className="Login_desktop bg-main w-100 vh-100 d-flex align-items-center justify-content-around d-none d-sm-flex">
+    <div className="card_wrapper ">
     <LoginCard/>
     </div>
-     <DashedCircle className="big_dashed_border" imageSrc="/assets/images/loginphoto.png"/>
-     <DashedCircle className="litle_dashed_border" imageSrc=""/>
+     <DashedCircle className="big_dashed_border" />
+     <DashedCircle className="litle_dashed_border"/>
+          <div className="image_wrapper d-flex justify-content-center position-relative">
+            <Image
+              src="/assets/images/loginphoto.png"
+              alt="vitsell-login"
+               width={467}
+              height={430}
+            />
+          </div>
     </section>
 
-<section className="d-block d-sm-none">
+      <section className="d-block d-sm-none">
   <FluidLayout>
     <Row>
     <section className="d-flex justify-content-center px-3 py-4 w-100">
@@ -26,7 +35,7 @@ const Login = () => {
     </section>
     </Row>
   </FluidLayout>
-    </section>
+      </section>
 
   </>
   );
